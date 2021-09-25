0f9244da773ee28cbf5ef798ac1af271ba9503db
mayurathan22 <thavaranjanmayurathan95@gmail.com>
2021-09-25 18:53:18 +0530

    show _modifications

--- src/Show.js
+++ src/Show.js
@@ -1,67 +1,67 @@
-// import React, { useState ,useEffect } from "react";
-// import Axios from 'axios';
-// import './show.css';
-// function Show() {
-//   const [emplist, setEmplist] = useState([]);
+import React, { useState ,useEffect } from "react";
+import Axios from 'axios';
+import './show.css';
+function Show() {
+  const [emplist, setEmplist] = useState([]);
 
-//   const showDetails=()=>{
+  const showDetails=()=>{
   
-//     // event =>  window.location.href='/show'
-//     Axios.get('http://localhost:2021/show').then((Response)=>{
-//       console.log(Response.data);
-//       // console.log(data);
+    // event =>  window.location.href='/show'
+    Axios.get('http://localhost:2021/show').then((Response)=>{
+      console.log(Response.data);
+      // console.log(data);
 
-//       setEmplist(Response.data);
-//     })
-//   };
+      setEmplist(Response.data);
+    })
+  };
 
-//    useEffect(() => {
-//     console.log("useeffect");
-//     showDetails();
-//   }, []);
+   useEffect(() => {
+    console.log("useeffect");
+    showDetails();
+  }, []);
 
-//   const deleteEmployee=(id)=>{
-//     Axios.delete(`http://localhost:2021/delete/+${id}`).then((Response)=>{
-//       setEmplist(emplist.filter((val)=>{
-//         return val.id !=id;
-//       }))
-//     })
-//   };
+  const deleteEmployee=(id)=>{
+    Axios.delete(`http://localhost:2021/delete/+${id}`).then((Response)=>{
+      setEmplist(emplist.filter((val)=>{
+        return val.id !=id;
+      }))
+    })
+  };
   
 
 
-//   return (
-//       <div >
-//         {showDetails}
-//       <table className="table">
-//         <tr>
-//           <th>Name</th>
-//           <th>Age</th>
-//           <th>Qualification</th>
-//           <th>Salary</th>
-//           <th>
+  return (
+      <div >
+        {showDetails}
+      <table className="table">
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Qualification</th>
+          <th>Salary</th>
+          <th>
 
-//           </th>
-//           <button className="back-btn" onClick={event =>  window.location.href='/'}>BACK</button>
-//         </tr>
-//         {emplist.map((val,key)=>{
-//             return(
-//                 <tr>
-//                 <td>{val.name}</td>
-//                 <td>{val.age}</td>
-//                 <td>{val.qualification}</td>
-//                 <td>{val.salary}</td>
-//                 <td>
-//                   <button className="btn-edit" onClick={event =>  window.location.href='/{id}/update'}>edit</button>
-//                   <button className="btn-delete" onClick={()=>{deleteEmployee(val.id)}}>delete</button>
-//                     {console.log(val.id)}
-//                 </td>
-//                 </tr>
-//             )
-//         })}
-//       </table>
-//     </div>
-//   );
-// }
+          </th>
+          <button className="back-btn" onClick={event =>  window.location.href='/'}>BACK</button>
+        </tr>
+        {emplist.map((val,key)=>{
+            return(
+                <tr>
+                <td>{val.name}</td>
+                <td>{val.age}</td>
+                <td>{val.qualification}</td>
+                <td>{val.salary}</td>
+                <td>
+                  <button className="btn-edit" onClick={event =>  window.location.href='/{id}/update'}>edit</button>
+                  <button className="btn-delete" onClick={()=>{deleteEmployee(val.id)}}>delete</button>
+                    {console.log(val.id)}
+                </td>
+                </tr>
+            )
+        })}
+      </table>
+    </div>
+  );
+}
 
-// export default Show;
+export default Show;
